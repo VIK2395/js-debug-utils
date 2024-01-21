ae8bfb96a3c4b807d731c455d23372610a1a693e
viktor.chupak <50545334+VIK2395@users.noreply.github.com>
2024-01-21 15:06:45 +0200

    Update consoleDir.ts

--- consoleDir.ts
+++ consoleDir.ts
@@ -1,6 +1,17 @@
 // https://stackoverflow.com/questions/10729276/how-can-i-get-the-full-object-in-node-jss-console-log-rather-than-object
 // https://nodejs.org/api/console.html
 
-export function consoleDir(elem: any): void {
-  console.dir(elem, { depth: null, colors: true, showHidden: false });
+type OptionsType = {
+  depth: number | null,
+  colors: boolean,
+  showHidden = boolean,
+}
+
+export function consoleDir(elem: any, options: OptionsType): void {
+  const customDefaultOptions: OptionsType = {
+    depth: 6,
+    colors: true,
+    showHidden: false
+  }
+  console.dir(elem, { ...customDefaultOptions, ...options });
 }
