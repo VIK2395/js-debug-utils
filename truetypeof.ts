b71c9715e0a5daab87ccb71472c178c0ed55c9b1
viktor.chupak <50545334+VIK2395@users.noreply.github.com>
2024-06-16 22:49:30 +0300

    Update truetypeof.ts

--- truetypeof.ts
+++ truetypeof.ts
@@ -1,6 +1,6 @@
 // https://learn.javascript.ru/instanceof#bonus-object-prototype-tostring-vozvraschaet-tip
 
-type TrueType =
+type RealType =
   'object' |
   'array' |
   'function' |
@@ -13,7 +13,7 @@ type TrueType =
   'boolean' |
   'nan'
 
-export function truetypeof(elem: any): TrueType {
+export function typeOf(elem: any): RealType {
     const type = Object.prototype.toString.call(elem).slice(8, -1).toLowerCase();
     if (type === 'number' && Number.isNaN(elem)) return 'nan';
     return type;
