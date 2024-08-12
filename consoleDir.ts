158ab222f769601881da3010db24d11c81769300
viktor.chupak <50545334+VIK2395@users.noreply.github.com>
2024-08-12 17:49:45 +0300

    Update consoleDir.ts

--- consoleDir.ts
+++ consoleDir.ts
@@ -19,5 +19,6 @@ export function consoleDir(elem: any, options: OptionsType): void {
 
 // To log object getters it is required to set showHidden to true as well
 // https://stackoverflow.com/questions/28072671/how-can-i-get-console-log-to-output-the-getter-result-instead-of-the-string-ge
+// https://nodejs.org/dist/latest-v12.x/docs/api/util.html#util_util_inspect_object_options
 console.dir(object, { getters: true, showHidden: true });
 console.log(util.inspect(object, { getters: true, showHidden: true }))
