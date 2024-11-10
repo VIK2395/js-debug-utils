ea1e33fc60c29978700a9c98ceb81da40658f49c
viktor.chupak <50545334+VIK2395@users.noreply.github.com>
2024-11-10 16:31:16 +0200

    Update delay.js

--- delay.js
+++ delay.js
@@ -1,1 +1,8 @@
+export async function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
+export function delaySync(ms) {
+  const end = Date.now() + ms;
+  while (Date.now() < end) {};
+}
